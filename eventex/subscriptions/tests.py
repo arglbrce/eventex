47ccd65444fa4cf167ccf90b90f4ffb50046c26a
Allan Lima <arglbr@gmail.com>
2019-06-20 20:22:00 -0300

    Realiza inscrição.

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,4 +1,6 @@
+from django.core import mail
 from django.test import TestCase
+
 from eventex.subscriptions.forms import SubscriptionForm
 
 
@@ -6,16 +8,14 @@ class SubscribeTest(TestCase):
     def setUp(self):
         self.response = self.client.get('/inscricao/')
 
-
     def test_get(self):
         """Get /inscricao/ must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
-
     def test_template(self):
         """Must use subscriptions/subscription_form.html"""
-        self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
-
+        self.assertTemplateUsed(
+            self.response, 'subscriptions/subscription_form.html')
 
     def test_html(self):
         """Html must contain input tags"""
@@ -25,19 +25,93 @@ class SubscribeTest(TestCase):
         self.assertContains(self.response, 'type="email"')
         self.assertContains(self.response, 'type="submit"')
 
-
     def test_csrf(self):
         """Html must contains csrf"""
         self.assertContains(self.response, 'csrfmiddlewaretoken')
-
 
     def test_has_form(self):
         """Context must have subscription form"""
         form = self.response.context['form']
         self.assertIsInstance(form, SubscriptionForm)
 
-
     def test_form_has_fields(self):
         """Must have 4 fields."""
         form = self.response.context['form']
-        self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
+        self.assertSequenceEqual(
+            ['name', 'cpf', 'email', 'phone'], list(form.fields))
+
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(name='Allan Lima', cpf='45773653320',
+                    email='arglbr@gmail.com', phone='85-98872-8779')
+        self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        """ Valid POST should redirect to /inscricao/"""
+
+        # HTTP 302: redirect
+        self.assertEqual(302, self.resp.status_code)
+
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, email.subject)
+
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, email.from_email)
+
+    def test_subscription_email_to(self):
+        email = mail.outbox[0]
+        expect = ['contato@eventex.com.br', 'arglbr@gmail.com']
+
+        self.assertEqual(expect, email.to)
+
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+
+        # nome
+        self.assertIn('Allan Lima', email.body)
+        # cpf
+        self.assertIn('45773653320', email.body)
+        # email
+        self.assertIn('arglbr@gmail.com', email.body)
+        # fone
+        self.assertIn('85-98872-8779', email.body)
+
+
+class SubscribeInvalidPost(TestCase):
+    def setUp(self):
+        self.resp = self.client.post('/inscricao/', {})
+
+    def test_post(self):
+        """Invalid POST should not redirect."""
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(
+            self.resp, 'subscriptions/subscription_form.html')
+
+    def test_has_form(self):
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_errors(self):
+        form = self.resp.context['form']
+        self.assertTrue(form.errors)
+
+
+class SubscribeSucessMessage(TestCase):
+    def test_message(self):
+        data = dict(name='Allan Lima', cpf='45773653320',
+                    email='arglbr@gmail.com', phone='85-98872-8779')
+        response = self.client.post('/inscricao/', data, follow=True)
+        self.assertContains(response, 'Inscrição realizada com sucesso!')
+
